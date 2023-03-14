d94585ce528cc6fe8565d3c980ddb2f81a571443
Thalles Yan Santos Medrado <ysm.thalles@gmail.com>
2023-03-14 15:43:02 -0300

    restore resnet backbone

--- networks/hmsanet.py
+++ networks/hmsanet.py
@@ -1,7 +1,6 @@
 import torch.nn as nn
 from torchvision import models
 
-from networks.segnet import SegNet
 from networks.utils import DoubleConv2dBnReluConv1x1, resize, scale_as
 
 
@@ -10,36 +9,40 @@ class AttentionHead(nn.Module):
         super().__init__()
 
 
-class HMSANet(SegNet):
+class HMSANet(nn.Module):
     def __init__(self, in_channels, out_channels, pretrained=True):
-        super().__init__(in_channels, out_channels, pretrained=pretrained)
+        super().__init__()
 
         self._training_low_scale_factor = 0.5
         self._evaluation_scale_factors = [0.25, 0.5, 1.0, 2.0]
 
+        # Backbone
+        resnet = models.resnet18(pretrained=pretrained)
+        self.backbone = nn.Sequential(
+            nn.Sequential(
+                resnet.conv1 if in_channels == 3 else nn.Conv2d(in_channels, 64, 7, stride=2, padding=3, bias=False),
+                resnet.bn1,
+                resnet.relu,
+                resnet.maxpool
+            ),
+            resnet.layer1,
+            resnet.layer2,
+            resnet.layer3,
+            resnet.layer4
+        )
+        backbone_out_channels = 512
+
         # Attention Head
         self.attention_head = nn.Sequential(
-            DoubleConv2dBnReluConv1x1(64, 256, 1),
+            DoubleConv2dBnReluConv1x1(backbone_out_channels, 256, 1),
             nn.Sigmoid()
         )
 
-    def _segnet_features(self, x):
-        encoder_activation_sizes = []
-        encoder_pooling_indices = []
-
-        for block in self.encoder:
-            x = block(x)
-            encoder_activation_sizes.append(x.size())
-            x, indices = self.pool(x)
-            encoder_pooling_indices.append(indices)
-
-        for i, block in enumerate(self.decoder):
-            x = self.unpool(x, indices=encoder_pooling_indices[-1-i], output_size=encoder_activation_sizes[-1-i])
-            x = block(x)
-        return x
+        # Semantic Head
+        self.head = DoubleConv2dBnReluConv1x1(backbone_out_channels, 256, out_channels)
 
     def _forward(self, x):
-        features = self._segnet_features(x)
+        features = self.backbone(x)
         attention = self.attention_head(features)
 
         features = scale_as(features, x)
