7077d9b36b69d7d0fe752b7f3d2881238dbea83b
Thalles Yan Santos Medrado <ysm.thalles@gmail.com>
2023-03-14 16:15:04 -0300

    add resnet for semantic segmentation

--- networks/__init__.py
+++ networks/__init__.py
@@ -1,4 +1,5 @@
 from networks.hmsanet import HMSANet
+from networks.resnet import ResNet
 from networks.segnet import SegNet
 from networks.unet import UNet
 
@@ -9,6 +10,8 @@ __all__ = [HMSANet, SegNet, UNet]
 def get_model(architecture, in_channels, out_channels, pretrained):
     if architecture == "HMSANet":
         return HMSANet(in_channels, out_channels, pretrained=pretrained)
+    elif architecture == "ResNet":
+        return ResNet(in_channels, out_channels, pretrained=pretrained)
     elif architecture == "SegNet":
         return SegNet(in_channels, out_channels, pretrained=pretrained)
     elif architecture == "UNet":
---
+++ networks/resnet.py
@@ -0,0 +1,31 @@
+import torch.nn as nn
+from torchvision import models
+
+from networks.utils import DoubleConv2dBnReluConv1x1
+
+
+class ResNet(nn.Module):
+    def __init__(self, in_channels, out_channels, pretrained=True):
+        super().__init__()
+
+        # Backbone
+        resnet = models.resnet18(pretrained=pretrained)
+        self.backbone = nn.Sequential(
+            nn.Sequential(
+                resnet.conv1 if in_channels == 3 else nn.Conv2d(in_channels, 64, 7, stride=2, padding=3, bias=False),
+                resnet.bn1,
+                resnet.relu,
+                resnet.maxpool
+            ),
+            resnet.layer1,
+            resnet.layer2,
+            resnet.layer3,
+            resnet.layer4
+        )
+        backbone_out_channels = 512
+
+        # Semantic Head
+        self.head = DoubleConv2dBnReluConv1x1(backbone_out_channels, 256, out_channels)
+
+    def forward(self, x):
+        return self.head(self.backbone(x))
